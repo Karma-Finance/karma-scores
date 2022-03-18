2c4d53d48189f580a3b98b91f185981b8d72bd8b
ICONationDevTeam <iconation.team@gmail.com>
2022-03-18 14:44:07 +0100

    Set OMM/IUSDC bond info

--- scripts/scenario/configs/omm-iusdc.py
+++ scripts/scenario/configs/omm-iusdc.py
@@ -1,6 +1,6 @@
 import json
 
-OMM_IUSDC_LP = "cx648a6d9c5f231f6b86c0caa9cc9eff8bd6040999"
+OMM_IUSDC = "cx648a6d9c5f231f6b86c0caa9cc9eff8bd6040999"
 OMM_IUSDC_LP_POOL_ID = 35
 OMM = "cxb94c0da9a8e650cc09b2f6b1bbfaa957ab47ec4c"
 
@@ -21,7 +21,7 @@ config = {
     "default": {
       # Type: Address
       # Principal and Payout tokens addresses
-      "principalToken": OMM_IUSDC_LP,
+      "principalToken": OMM_IUSDC,
       "payoutToken": OMM,
 
       # Type: Address
@@ -30,12 +30,12 @@ config = {
 
       # Type: Integer
       # Vesting term value (in seconds)
-      "vestingTermSeconds": 604800, # 7 * 24h * 3600s = 1 week
+      "vestingTermSeconds": 129600, # 7 * 24h * 3600s = 1 week
 
       # Array of fees tiers, in ten-thousandths (i.e. 33300 = 3.33%)
       "fees": [
         hex(33300), # = 3.33%
-        hex(66600), # = 6.66%
+        hex(33300), # = 3.33%
       ],
 
       # Array of ceilings of principal bonded till next tier
@@ -48,8 +48,8 @@ config = {
         "controlVariable": hex(60000),
         "minimumPrice": hex(0),
         "maxPayout": hex(500),
-        "maxDebt": hex(10**18),
-        "initialDebt": hex(6001 * 10**18),
+        "maxDebt": hex(10001000000000000000000),
+        "initialDebt": hex(6001000000000000000000),
       },
     },
 
@@ -66,12 +66,12 @@ config = {
       # Type: Address
       # Initial owner of the custom treasury
       "initialOwner": "hxb6b5791be0b5ef67063b3c10b840fb81514db2fd",
-      
+
       # Type: String (hexstring)
-      # Initial funding of the custom treasury
+      # Initial funding of the custom treasury payout token
       # This value must be a hexstring
       # If "0x0", do not send anything
-      "initialPayoutFunding": hex(1 * 10**18), # 1 sICX
+      "initialPayoutFunding": hex(20000 * 10**18), # 20k OMM
     },
   },
 }
